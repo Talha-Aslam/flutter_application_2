51cc897fb88bc88fad090e41d44d7710204b9192
Talha Aslam <ta104g38@gmail.com>
2024-07-13 18:20:00 +0500

    new files

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/data/question_list.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,70 +13,47 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionPageState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
+    final currenQuestion = questions[0];
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'This is the First questions',
-            style: TextStyle(
-              fontSize: 30,
-              color: Colors.white,
-            ),
-          ),
-          const SizedBox(height: 30),
-          ElevatedButton(
-            onPressed: () {},
-            style: TextButton.styleFrom(
-                backgroundColor: Color.fromARGB(255, 164, 109, 231),
-                foregroundColor: const Color.fromARGB(255, 255, 255, 255)),
-            child: Text(
-              "Answers 1",
+      child: Container(
+        margin: EdgeInsets.all(50),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              currenQuestion.text,
               style: TextStyle(
-                fontSize: 15,
+                fontSize: 30,
+                color: Colors.white,
               ),
+              textAlign: TextAlign.center,
             ),
-          ),
-          const SizedBox(height: 10),
-          ElevatedButton(
-            onPressed: () {},
-            style: TextButton.styleFrom(
-                backgroundColor: Color.fromARGB(255, 164, 109, 231),
-                foregroundColor: const Color.fromARGB(255, 255, 255, 255)),
-            child: Text(
-              "Answers 2",
-              style: TextStyle(
-                fontSize: 15,
-              ),
-            ),
-          ),
-          const SizedBox(height: 10),
-          ElevatedButton(
-            onPressed: () {},
-            style: TextButton.styleFrom(
-                backgroundColor: Color.fromARGB(255, 164, 109, 231),
-                foregroundColor: const Color.fromARGB(255, 255, 255, 255)),
-            child: Text(
-              "Answers 3",
-              style: TextStyle(
-                fontSize: 15,
-              ),
-            ),
-          ),
-          const SizedBox(height: 10),
-          ElevatedButton(
-            onPressed: () {},
-            style: TextButton.styleFrom(
-                backgroundColor: Color.fromARGB(255, 164, 109, 231),
-                foregroundColor: const Color.fromARGB(255, 255, 255, 255)),
-            child: Text(
-              "Answers 4",
-              style: TextStyle(
-                fontSize: 15,
-              ),
-            ),
-          ),
-        ],
+            const SizedBox(height: 30),
+            ...currenQuestion.answers.map((answers) {
+              return Container(
+                margin: const EdgeInsets.only(top: 15),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 164, 109, 231),
+                    foregroundColor: const Color.fromARGB(255, 255, 255, 255),
+                    padding: const EdgeInsets.fromLTRB(60, 0, 60, 0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                  ),
+                  child: Text(
+                    currenQuestion.answers[0],
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+              );
+            }),
+          ],
+        ),
       ),
     );
   }
